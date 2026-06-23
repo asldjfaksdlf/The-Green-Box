763ad16f4aa143e37d7880a50b210e74b1a86fef
MyProductions <josh_skalski@yahoo.com>
2026-06-22 19:39:26 -0700

    Delete The Green Box directory

--- The Green Box/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GreenBoxApp from "./GreenBox.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GreenBoxApp />
-  </React.StrictMode>
-);
